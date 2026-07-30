473dff58998a69a490137a2711d6760cde8f2ac0
Tejas Pawar <pawartejas2307@gmail.com>
2026-07-30 19:51:43 +0530

    First Program of Javascript

---
+++ 3.Javascript/1.First-Program.js
@@ -0,0 +1,22 @@
+console.log("Hello World.....!");
+
+let age = 19;
+
+const Product = {
+    title : "Ball Pen",
+    rating : 4,
+    offer : 5,
+    price : 270,
+
+};
+
+console.log(Product)
+
+const Profile = {
+    username : "Tejas Pawar",
+    isFollow : false,
+    followers : 100000,
+    following : 1,
+};
+
+console.log(Profile)
