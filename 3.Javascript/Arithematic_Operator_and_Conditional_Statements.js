d951dd63140bfc828c229a487d9c575406ab790c
Tejas Pawar <pawartejas2307@gmail.com>
2026-08-03 20:06:18 +0530

    Learn about Conditional Statements

---
+++ 3.Javascript/Arithematic_Operator_and_Conditional_Statements.js
@@ -0,0 +1,39 @@
+// Arithemetic Operators
+
+let a = 6;
+let b = 6;
+
+let sum = a+b;
+let diff = a-b;
+let product = a*b;
+let division = a/b;
+let expo = a**b
+let remaindar = a%b;
+
+
+console.log("Your sum is :",sum);
+console.log("Your diff is :",diff);
+console.log("Your product is :",product);
+console.log("Your division is :",division);
+console.log("Your expo is :",expo);
+console.log("Your remaindar is :",remaindar);
+
+// Conditional Statements
+
+let age = 19;
+
+if (age>18) {
+    console.log("You can Vote");
+} else if (age<18) {
+    console.log("You can not Vote");
+} else {
+    console.log("You are alien");
+}
+
+let color = "Red";
+
+if (color === "Red") {
+    console.log("It's a Same Color");
+} else {
+    console.log("It's Not Same Color");
+}
