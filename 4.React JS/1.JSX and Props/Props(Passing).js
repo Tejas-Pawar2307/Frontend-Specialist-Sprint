145e1111b18413726f667e4e694a00830bbb42cb
Tejas Pawar <pawartejas2307@gmail.com>
2026-08-24 23:03:49 +0530

    Learn about passing the Props through App.js

---
+++ 4.React JS/1.JSX and Props/Props(Passing).js
@@ -0,0 +1,21 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import './output.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+function App() {
+
+    
+
+  return (
+    <>
+
+        <Navbar title={"Home"} mode={mode} toggleMode={toggleMode}/>
+        
+
+    </>
+    );
+}
+
+export default App;
